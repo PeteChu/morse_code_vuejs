e76eba0e539e930ba4092d328daa2de164297788
SchecterZa <phuengton.ch.57@ubu.ac.th>
2019-01-25 16:40:38 +0700

    add project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <MorseCode/>
+  </div>
+</template>
+
+<script>
+import MorseCode from "./components/MorseCode";
+
+export default {
+  name: "App",
+  components: { MorseCode }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MorseCode.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Hello Morse Code</h1>
+    <h3>Message</h3>
+    <input type="text" v-model="message" v-on:input="toMorse(message)">
+    <h3>Encoded Message</h3>
+    <h4>{{morseEncoded}}</h4>
+  </div>
+</template>
+
+<script>
+import morseJson from "./../../static/morseCode.json";
+export default {
+  name: "MorseCode",
+  data: function() {
+    return {
+      morseCode: morseJson,
+      message: "",
+      morseEncoded: ""
+    };
+  },
+  methods: {
+    toMorse: function(s) {
+      this.morseEncoded = "";
+      for (var i of s.toLowerCase()) {
+        if (i == " ") {
+          this.morseEncoded += "/ ";
+        } else {
+          this.morseEncoded += this.morseCode[i] + " ";
+        }
+      }
+    }
+  }
+};
+</script>
+
